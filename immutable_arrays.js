eeff7c9ef93d33deae1d114a4618e98d862e16e6
Miralem.Ba <miralembanovi@gmail.com>
2024-02-17 15:43:45 +0100

    Array with objects, including unique identifiers

---
+++ immutable_arrays.js
@@ -0,0 +1,6 @@
+// Array mit Objekten, einschließlich eindeutiger Identifikatoren
+const objectsArray = [
+    { id: 1, name: 'Objekt1', attribute: 'Wert1' },
+    { id: 2, name: 'Objekt2', attribute: 'Wert2' }
+  ];
+  

d5fb56dc674547d058de69be63927fd9258c3874
Miralem.Ba <miralembanovi@gmail.com>
2024-02-17 15:48:11 +0100

    New array with additional element

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -1,6 +1,13 @@
 // Array mit Objekten, einschließlich eindeutiger Identifikatoren
-const objectsArray = [
+const objects = [
     { id: 1, name: 'Objekt1', attribute: 'Wert1' },
     { id: 2, name: 'Objekt2', attribute: 'Wert2' }
   ];
   
+//Neues Array mit zusätzlichem Element
+const newArrayWithExtraElement = [
+    ...objects,
+    { id: 3, name: 'Objekt3', attribute: 'Wert3' }
+  ];
+
+//

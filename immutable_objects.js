20b78aa4154db4f9e91ea1dbe5ba7064993f0176
Miralem.Ba <miralembanovi@gmail.com>
2024-02-17 15:19:15 +0100

    Create a constant variable that contains an object as a value and has at least 3 properties.

--- immutable_objects.js
+++ immutable_objects.js
@@ -1,2 +1,12 @@
 //Erstellen Sie eine Variable, welche einen primitiven Datentyp abbildet und nicht neu zugewiesen werden kann.
 const number = 42;
+
+
+//Erstellen Sie eine konstante Variable, welche ein Objekt als Wert enthält und mindestens 3 Eigenschaften aufweist.
+const person = {
+    name: "Miralem Banovi",
+    age: 27,
+    occupation: "Software Developer"
+  };
+  
+// 

eaeeae0d1e06bfd58557399858752cd39343b6fd
Miralem.Ba <miralembanovi@gmail.com>
2024-02-17 15:59:39 +0100

    New array without the element with the smallest ID. and Calculation of the average of the values

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -9,5 +9,13 @@ const newArrayWithExtraElement = [
     ...objects,
     { id: 3, name: 'Objekt3', attribute: 'Wert3' }
   ];
+  
 
-//
+//Neues Array ohne das Element mit dem kleinsten ID
+const newArrayWithoutSmallestId = objects.filter(obj => obj.id !== 1);
+
+
+//Berechnung des Durchschnitts der Werte
+const reviews = [4.5, 4.0, 5.0, 2.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5, 3.0, 2.5, 2.0];
+const averageReview = reviews.reduce((acc, value) => acc + value, 0) / reviews.length;
+
